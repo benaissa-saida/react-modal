4842cdacf73080463a84c02284f10d98d96f1a2d
benaissa-saida <benaissasaida28@gmail.com>
2023-06-26 16:45:01 +0200

    first-commit

---
+++ src/lib/Modal.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./modal.css";
+
+function Modal({ title, closeModal, isOpened }) {
+  return (
+    <div>
+      {isOpened && (
+        <div className="modal-window">
+          <div className="modal-body">
+            <span>{title}</span>
+            <button onClick={closeModal} className="modal-close"></button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Modal;
